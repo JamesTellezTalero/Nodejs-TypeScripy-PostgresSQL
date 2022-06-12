867835b7b5f5b164b816696fe90a81e3139b7170
Ex <jtalero10@outlook.es>
2022-06-11 20:30:13 -0500

    crud terminada

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express';
-
 const app = express();
-const port = 8080;
+const port = 8000;
+
+//middleware
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//routes
+import indexRoutes from './routes/index';
+
+app.use(indexRoutes);
+
 
 app.listen(port, ()=>{
     console.log(`Server on port: ${port}`)
